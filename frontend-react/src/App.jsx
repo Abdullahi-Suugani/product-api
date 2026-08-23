d6ed75c3a90028ee8d311c221939646067bc8e83
Abdullahi <abdullahi45230@student.simad.edu.so>
2026-08-23 21:23:53 +0300

    Add

--- frontend-react/src/App.jsx
+++ frontend-react/src/App.jsx
@@ -1,36 +1,38 @@
 import { useEffect, useState } from "react";
 import ProductCard from "./components/ProductsCard";
 
-
 function App() {
-  const [products, setProducts] = useState([
-    { id: 1, name: "laptop", price: 800 },
-    { id: 2, name: "keyboard", price: 90 },
-    { id: 3, name: "mouse", price: 20 },
-  ]);
-
+  const [products, setProducts] = useState([]);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     fetch("http://localhost:5000/products")
       .then((response) => response.json())
       .then((data) => {
         setProducts(data);
+        setLoading(false);
       })
       .catch((error) => {
         console.error("Error fetching products:", error);
+        setLoading(false);
       });
   }, []);
 
+  if (loading) {
+    return <h1>Loading products...</h1>;
+  }
+
   return (
     <div>
-      <h1> My products</h1>
-      {products.map((product) => {
+      <h1>My Products</h1>
+
+      {products.map((product) => (
         <ProductCard
           key={product.id}
           name={product.name}
           price={product.price}
-        />;
-      })}
+        />
+      ))}
     </div>
   );
 }
